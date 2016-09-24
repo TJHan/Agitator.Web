afcdc44a9cd322b9df8ccd246e669072e0fd97a0
hantongjun7@163.com <hantongjun7@163.com>
2016-09-25 00:05:49 +0800

    提交git遗漏的文件

---
+++ Agitator.Business/Filters/AuthAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web;
+using Agitator.Business.Common;
+using Rock.Common.Helper;
+
+namespace Agitator.Business.Filters
+{
+    public class AuthAttribute:AuthorizeAttribute
+    {
+        public AuthAttribute(string moduleKey)
+        {
+            this.webSiteModuleRightsKey = moduleKey;
+        }
+
+        private string webSiteModuleRightsKey = string.Empty;
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (!RequestUser.CurrentRequestUser.IsLogin)
+                return false;
+
+            return CheckAuthorizedModules();
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+
+            HttpContext.Current.Response.Redirect(RequestUser.CurrentRequestUser.LoginURL);
+            HttpContext.Current.Response.SuppressContent = false;
+            HttpContext.Current.ApplicationInstance.CompleteRequest();
+        }
+
+        private bool CheckAuthorizedModules()
+        {
+            var module = ModuleMapping.ModuleMappingDict.Where(d => d.Value.RightsKey.Equals(webSiteModuleRightsKey)).FirstOrDefault();
+            var result = RequestUser.CurrentRequestUser.LoginUserMenuList.Where(d => d.id == module.Key.ToInt());
+            if (result != null && result.FirstOrDefault() != null)
+            {
+
+                return true;
+            }
+            return false;
+        }
+    }
+}
